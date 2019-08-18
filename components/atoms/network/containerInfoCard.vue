d13c711d233db54a927d460eba7dd19ace2b3f8d
yuu3 <yunosuke0802@icloud.com>
2019-08-19 02:23:54 +0900

    追加

---
+++ components/atoms/network/containerInfoCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="containerInfo-card"
+   :style="`background: ${info.color};`">
+    <h1>{{ info.title }}:</h1>
+    <div class="containerInfo-card__data">{{ info.data }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class ContainerInfoCard extends Vue {
+  @Prop() info!: object
+}
+</script>
